113b5060ceab4d737c762ec78b5fda0041a77c98
Diego Benedetti <diegobenedetti49@gmail.com>
2026-06-18 10:18:05 -0300

    feat: alterado projeto npm run

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -12,6 +12,9 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
   },
+  turbopack: {
+    root: '../../',
+  },
 }
 
 export default nextConfig
